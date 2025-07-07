8c139950293d15aaa89c54ceae0fda1debe32826
Rhymesdavi <dokeyo390@gmail.com>
2025-07-07 11:52:05 +0300

    Troubleshoot appwrite configurations

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,5 +1,6 @@
 import CustomButton from "@/components/CustomButton";
 import CustomInput from "@/components/CustomInput";
+import { createUser } from "@/lib/appwrite";
 import { Link, router } from "expo-router";
 import React, { useState } from "react";
 import { Alert, Text, View } from "react-native";
@@ -9,12 +10,19 @@ const SignUp = () => {
   const [form, setForm] = useState({ name: "", email: "", password: "" });
 
   const submit = async () => {
-    if (!form.name || !form.email || !form.password) return Alert.alert("Error", "Please enter valid email address & password");
+    const { name, email, password } = form;
+
+    if (!name || !email || !password)
+      return Alert.alert(
+        "Error",
+        "Please enter valid email address & password"
+      );
 
     setIsSubmitting(true);
 
     try {
-      Alert.alert("Success", "User signed up successfully.");
+      await createUser({ email, password, name });
+
       router.replace("/");
     } catch (error: any) {
       Alert.alert("Error", error.message);
@@ -48,7 +56,7 @@ const SignUp = () => {
         label="Password"
         secureTextEntry={true}
       />
-      <CustomButton title="Sign In" isLoading={isSubmitting} onPress={submit} />
+      <CustomButton title="Sign Up" isLoading={isSubmitting} onPress={submit} />
 
       <View className="flex justify-center mt-5 flex-row gap-2">
         <Text className="base-regular text-gray-100">
