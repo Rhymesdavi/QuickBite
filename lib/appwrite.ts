9f2838f05fcbd07a420833ff5de59d8e35204525
Rhymesdavi <dokeyo390@gmail.com>
2025-07-23 15:23:46 +0300

    Implement Search and Filter

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -112,6 +112,8 @@ export const getCategories = async () => {
       appwriteConfig.databaseId,
       appwriteConfig.categoriesCollectionId
     )
+
+    return categories.documents
   } catch (e) {
     throw new Error(e as string)
   }
